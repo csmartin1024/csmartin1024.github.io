907ffc1fa761b611ece41110b877c41029ee7c42
Cody Martin <csmartin1024@gmail.com>
2018-07-29 12:29:00 -0700

    Test 9

--- src/App.js
+++ src/App.js
@@ -55,43 +55,13 @@ class App extends Component
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Cody's Site</h1>
+          <h1 className="App-title">Welcome to Cody's Site (being deployed via circle)</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         {this.render3()}
       </div>
-    );
-  }
-
-  render3() 
-  {
-    return (
-      <div>
-      <div>Device Test!</div>
-      <MediaQuery query="(min-device-width: 1224px)">
-        <div>You are a desktop or laptop</div>
-        <MediaQuery query="(min-device-width: 1824px)">
-          <div>You also have a huge screen</div>
-        </MediaQuery>
-        <MediaQuery query="(max-width: 1224px)">
-          <div>You are sized like a tablet or mobile phone though</div>
-        </MediaQuery>
-      </MediaQuery>
-      <MediaQuery query="(max-device-width: 1224px)">
-        <div>You are a tablet or mobile phone</div>
-      </MediaQuery>
-      <MediaQuery query="(orientation: portrait)">
-        <div>You are portrait</div>
-      </MediaQuery>
-      <MediaQuery query="(orientation: landscape)">
-        <div>You are landscape</div>
-      </MediaQuery>
-      <MediaQuery query="(min-resolution: 2dppx)">
-        <div>You are retina</div>
-      </MediaQuery>
-    </div>
     );
   }
 }
